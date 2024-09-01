42ee2e39208f16ddbbe6e9b113fb22e4a0d76b91
CK-N9ne <keighan.robichaudgriffin@maine.edu>
2024-09-01 11:03:51 -0400

    feat: added screen, and quit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import * 
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (0,0,0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color)
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
