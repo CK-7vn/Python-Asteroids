69b15c8b4f1f1d6ab24dc64a3cb9372c6aace649
CK-N9ne <keighan.robichaudgriffin@maine.edu>
2024-09-01 22:02:53 -0400

    feat: added rotation to the triangle

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
 from constants import * 
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0,0,0)
-    while True:
+    dt = 0
+    pyClock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+        dt = pyClock.tick(60) / 1000
         screen.fill(color)
+        #dt = returnValue / 1000
+         
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,31 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import *
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        self.rotation = 0
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    def draw(self, screen):
+        color = (255,255,255)
+        pygame.draw.polygon(screen, color, self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.rotate(-dt)
+        if keys[pygame.K_LEFT]:
+            self.rotate(dt)
